526ec5f319c1edc76ea09874a2b36820ec66e903
ваше имя или ник латиницей <dmitry.bazyk@yandex.ru>
2024-12-06 15:55:23 +0700

    Первый коммит

---
+++ home-react/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return React.createElement(
+    'div',
+    { className: 'App' },
+    React.createElement(
+      'header',
+      { className: 'App-header' },
+      React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+      React.createElement(
+        'p',
+        null,
+        'Edit ',
+        React.createElement('code', null, 'src/App.js'),
+        ' and save to reload.'
+      ),
+      React.createElement(
+        'a',
+        {
+          className: 'App-link',
+          href: 'https://reactjs.org',
+          target: '_blank',
+          rel: 'noopener noreferrer',
+        },
+        'Learn React'
+      ),
+      // Отображение текущего года
+      React.createElement('p', null, new Date().getFullYear()) // Текущий год
+    )
+  );
+}
+
+export default App;
+
+
+
